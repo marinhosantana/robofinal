bd94000a4d8abaad946bbcb92b3708bb9d3a9613
marinhosantana <marinhosantana10@gmail.com>
2022-10-31 06:54:58 -0300

    Recebi

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument("headless")
+driver = webdriver.Firefox()
+driver.get("https://etherscan.io/tx/0xcc22fc0c43eb8c6a88ce92fd344108cf8f690c21da57a3fb0d8dadcd207d5de3")
