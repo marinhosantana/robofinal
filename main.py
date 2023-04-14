36898c72a9c17fd1472b6b600cd68fbdacf05a6c
marinhosantana <65985873+marinhosantana@users.noreply.github.com>
2023-04-14 18:21:20 -0300

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-driver = webdriver.Firefox()
-driver.get("https://etherscan.io/tx/0xcc22fc0c43eb8c6a88ce92fd344108cf8f690c21da57a3fb0d8dadcd207d5de3")
